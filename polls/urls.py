c6c570fc6c22d966f2c3bffe86ec8f9b1f9c01f9
manuelvmart <manevergara14@gmail.com>
2025-01-23 13:36:01 -0600

    january tt

--- polls/urls.py
+++ polls/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/",include('django.contrib.auth.urls')),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:pk>/resultado/", views.ResultsView.as_view(), name="resultado"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('<int:question_id>/<int:answer_id>/cancel_answer/', views.cancel_answer, name='cancel_answer'),
+        
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,19 @@
 
-from django.shortcuts import get_object_or_404, render
+"""
+Vista para resolver peticiones de Polls
+"""
 from django.db.models import F
-from django.http import HttpResponseRedirect
 from django.views import generic
-from .models import Choice, Question,Answers
 from django.urls import reverse
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .models import Choice, Question,Answers
+
+
 class IndexView(generic.ListView):
+    """Vista para listar los polls"""
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
@@ -17,18 +22,57 @@ class IndexView(generic.ListView):
         return Question.objects.order_by("-pub_date")[:5]
 
 
-
-class DetailView(LoginRequiredMixin,generic.DetailView):
+class DetailView(LoginRequiredMixin, generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    # def dispatch(self, request, *args, **kwargs):
+    #     response = super().dispatch(request, *args, **kwargs)
+    #     if request.user.answers_set.filter(question_id=kwargs.get("pk")):
+    #         raise Http404
+    #     return response
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data.update({
+            "is_anwered": self.request.user.answers_set.filter(
+                question=context_data["question"]
+            ).exists()
+        })
+        # print(self.request.user.answers_set.filter(
+        #     question=context_data["question"]
+        # ).exists())
+        print(context_data)
+        return context_data
+    
+
+    
+        
+
+
 def detail(request, question_id):
+    model = Answers
     question = get_object_or_404(Question, pk=question_id)
     
     # Get all answers for the current question
     answers = Answers.objects.filter(question=question).select_related('user')
     
     return render(request, 'polls/detail.html', {'question': question, 'answers': answers})
+
+def cancel_answer(request, answer_id, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    answer = get_object_or_404(Answers, id=answer_id)
+    if request.user == answer.voter:
+        answer.delete()
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+   
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -66,3 +110,13 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+
+class ResultadoView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+
+
+
